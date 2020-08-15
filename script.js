cd22f88c36aad8de942e4c21c73c40acf1824107
piaspro <golamrahman09@gmail.com>
2020-08-15 18:38:28 +0600

    push

---
+++ script.js
@@ -0,0 +1,40 @@
+// Add event listener
+const searchBox = document.querySelector(".input");
+searchBox.addEventListener('keypress', getValue);
+
+function getValue(press){
+    if (press.keyCode == 13){
+        getResults(searchBox.value);
+    }
+}
+
+const btn = document.querySelector(".btn");
+btn.addEventListener('click', function(){
+    getResults(searchBox.value);
+});
+// fetch data
+function getResults(value){
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${value}&units=metric&APPID=53f5bcb9ee8e03a33c85f46690b4267c`)
+    .then(response => response.json())
+    .then (data => {
+        showResult(data);
+    })
+}
+// Show result
+function showResult(data) {
+    const location = document.getElementById("location");
+    location.innerHTML = ` <i class="fa fa-map-marker" style="font-size:36px;color:red"></i> ${data.name} ${data.sys.country}`;
+
+    const temperature = document.getElementById("temp");
+    temperature.innerHTML = `${Math.round(data.main.temp)} <span>°c</span>`;
+
+    const real_feel = document.getElementById("real-feel");
+    real_feel.innerHTML = `<span>Real_feel:</span> ${Math.round(data.main.feels_like)} <span>°c</span>`;
+
+    const condition = document.getElementById("condition");
+    condition.innerText = `${(data.weather[0].main)}`;
+    
+    const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    const image = document.getElementById("img");
+    image.setAttribute('src', iconUrl);
+}
